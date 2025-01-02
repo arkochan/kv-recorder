9b6671e075d2a71c3d1759c2a372fbd1709f9305
arkochan <najmussakibarko@gmail.com>
2025-01-03 00:03:08 +0600

    add ButtonTB component and remove ButtonIcon component

---
+++ src/components/ButtonTB.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from 'react';
+import { cn } from '@/lib/utils/tailwind';
+import Image from 'next/image';
+interface ButtonTBProps {
+    svgSrc: string;
+    label?: string;
+    onClick: () => void;
+    className?: string;
+}
+
+export default function ButtonTB({ svgSrc, label, onClick, className }: ButtonTBProps) {
+    return (
+        <button onClick={onClick} className={cn("flex items-center p-2 gap-2 bg-white rounded-md border border-grey border-opacity-0 hover:border-opacity-100", className)}>
+            <Image src={svgSrc} alt={label || svgSrc} width={24} height={24} />
+            {label && <span className="font-medium text-sm text-black">{label}</span>}
+        </button>
+    );
+}
