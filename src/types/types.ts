fadfa913aabb0f3230b62e6ede1ad1d912ecfc5d
arkochan <najmussakibarko@gmail.com>
2025-01-03 20:33:15 +0600

    Added new types for recording

--- src/types/types.ts
+++ src/types/types.ts
@@ -13,3 +13,28 @@ export interface Stroke {
 export interface Session {
   strokes: Stroke[];
 }
+
+
+export interface Eraser_path {
+  points: Point[];
+  color: string;
+  width: number;
+}
+
+// Define the Operation interface with Event details
+export interface Event {
+  type: string;
+  startTime: number;
+  Stroke?: Point[];
+  Eraser_path?: { points: Point[], radius: number };
+  rec_points?: Point;
+  circle_points?: Point[];
+}
+
+// Define the Session interface
+export interface Session {
+  author: string;
+  otherDetails: string;
+  details: string;
+  events: Event[];
+}
