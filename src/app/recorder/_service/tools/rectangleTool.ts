7f83204dba4a5828488a0f49a1686b45bd1e6e48
arkochan <najmussakibarko@gmail.com>
2025-01-08 07:46:07 +0600

    rectangle invert draw now fixed

--- src/app/recorder/_service/tools/rectangleTool.ts
+++ src/app/recorder/_service/tools/rectangleTool.ts
@@ -4,7 +4,7 @@ import { Tool } from "./tool";
 
 export class RectangleTool extends Tool {
 
-  private drawRectangle(p: Point) {
+  drawRectangle(p: Point) {
 
     const startPoint = this.whiteboard.points[0];
     if (!startPoint) return;
@@ -14,17 +14,15 @@ export class RectangleTool extends Tool {
 
     const x = Math.min(startPoint.x, p.x);
     const y = Math.min(startPoint.y, p.y);
-    const width = (startPoint.x - p.x);
-    const height = (startPoint.y - p.y);
+    const width = Math.abs(startPoint.x - p.x);
+    const height = Math.abs(startPoint.y - p.y);
 
-    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     ctx.strokeRect(x, y, width, height);
   }
 
 
 
   down(p: Point) {
-
     this.whiteboard.pointerDown(p);
     this.whiteboard.started = true;
   }
@@ -42,7 +40,7 @@ export class RectangleTool extends Tool {
     if (!this.whiteboard.started) return;
     this.clearMemCanvas();
     this.saveCanvas();
-
+    console.log("canvas Saved");
     this.whiteboard.pointerUp(p);
     this.whiteboard.started = false;
   }
