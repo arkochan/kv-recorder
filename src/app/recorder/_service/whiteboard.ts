025086cef131bc53f452e7f77aa633455619c60f
arkochan <najmussakibarko@gmail.com>
2025-01-08 18:42:30 +0600

    Uses Events array to store all events

--- src/app/recorder/_service/whiteboard.ts
+++ src/app/recorder/_service/whiteboard.ts
@@ -1,42 +1,49 @@
-import { Point, Stroke } from "@/types/types";
+import { Point, Stroke, Event } from "@/types/types";
 
 export class Whiteboard {
-  public points: Point[] = [];
   public started = false;
-  public strokes: Stroke[] = [];
+  public Events: Event[] = [];
   private smoothFactor = 4;
   public size = 0;
   private dpr = 2;
   public tool: string = "pen";
   public startTime: number = 0;
-
+  public currentEvent: Event | null = null;
   constructor({ smoothFactor }: { smoothFactor: number }) {
     this.smoothFactor = smoothFactor;
-    this.strokes = [];
-    this.points = [];
+    this.Events = [];
   }
 
   startSession(time: number) {
     this.startTime = time;
   }
 
+
   pointerDown(p: Point) {
-    this.points.push(p);
     this.size++;
+    this.currentEvent = {
+      type: this.tool,
+      startTime: p.time,
+      points: [p],
+      ...(this.tool === "pen" && { stroke: { width: 2, color: 'black' } })
+    };
   }
 
   pointerMove(p: Point) {
-    if (this.size % this.smoothFactor !== 0) {
-      this.points.pop();
+    if (this.tool === "pen" && this.size % this.smoothFactor !== 0) {
+      this.currentEvent?.points.pop();
     }
-    this.points.push(p);
+    this.currentEvent?.points.push(p);
     this.size++;
   }
+
   pointerUp(p: Point) {
     this.started = false;
-    this.points.push(p);
-    this.strokes.push({ points: this.points, width: 2, color: 'black' });
-    this.points = [];
+    if (this.currentEvent) {
+      this.currentEvent.points.push(p);
+      this.Events.push(this.currentEvent);
+      this.currentEvent.points = [];
+    }
     this.size = 0;
   }
 }
