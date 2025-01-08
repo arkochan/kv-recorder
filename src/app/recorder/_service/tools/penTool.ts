aaf480ff84b7a1f3308f9babcb7e18b62a5a5d4b
arkochan <najmussakibarko@gmail.com>
2025-01-08 09:12:33 +0600

    moved drawpoints from pentool to tool. also drawpoints now requires argument points arrawy

--- src/app/recorder/_service/tools/penTool.ts
+++ src/app/recorder/_service/tools/penTool.ts
@@ -3,28 +3,6 @@ import { Tool } from "./tool";
 
 export class PenTool extends Tool {
 
-  drawPoints() {
-    console.log("drawPoints", "executed");
-    console.log(this.whiteboard.points);
-    // draw a basic circle instead
-    if (!this.ctx) return;
-    // if (!this.canvas) return;
-    const points = this.whiteboard.points;
-    if (this.whiteboard.size < 2) {
-      var b = this.whiteboard.points[0];
-      this.ctx.beginPath(), this.ctx.arc(b.x, b.y, this.ctx.lineWidth / 2, 0, Math.PI * 2, !0), this.ctx.closePath(), this.ctx.fill();
-      return
-    }
-
-    this.ctx.beginPath(), this.ctx.moveTo(points[0].x, points[0].y);
-    // draw a bunch of quadratics, using the average of two points as the control point
-    for (let i = 1; i < points.length - 1; i++) {
-      var c = (points[i].x + points[i + 1].x) / 2,
-        d = (points[i].y + points[i + 1].y) / 2;
-      this.ctx.quadraticCurveTo(points[i].x, points[i].y, c, d)
-    }
-    this.ctx.stroke()
-  }
 
   down(p: Point) {
     this.whiteboard.pointerDown(p);
@@ -36,7 +14,7 @@ export class PenTool extends Tool {
     this.whiteboard.pointerMove(p);
     this.clearCanvas();
     this.putMemCanvas();
-    this.drawPoints();
+    this.drawPoints(this.whiteboard.points);
 
   }
 
@@ -44,7 +22,7 @@ export class PenTool extends Tool {
     if (!this.whiteboard.started) return;
     this.clearCanvas();
     this.putMemCanvas();
-    this.drawPoints();
+    this.drawPoints(this.whiteboard.points);
     this.clearMemCanvas();
     this.saveCanvas();
 
