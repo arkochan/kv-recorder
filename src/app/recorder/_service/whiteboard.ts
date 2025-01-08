6d79bd849703fda4c92bbbb81286a4b3e2b12d1f
arkochan <najmussakibarko@gmail.com>
2025-01-08 19:25:36 +0600

    Normal points are back no currentEvent Shit

--- src/app/recorder/_service/whiteboard.ts
+++ src/app/recorder/_service/whiteboard.ts
@@ -1,6 +1,7 @@
 import { Point, Stroke, Event } from "@/types/types";
 
 export class Whiteboard {
+  public points: Point[] = [];
   public started = false;
   public Events: Event[] = [];
   private smoothFactor = 4;
@@ -8,7 +9,6 @@ export class Whiteboard {
   private dpr = 2;
   public tool: string = "pen";
   public startTime: number = 0;
-  public currentEvent: Event | null = null;
   constructor({ smoothFactor }: { smoothFactor: number }) {
     this.smoothFactor = smoothFactor;
     this.Events = [];
@@ -21,29 +21,28 @@ export class Whiteboard {
 
   pointerDown(p: Point) {
     this.size++;
-    this.currentEvent = {
-      type: this.tool,
-      startTime: p.time,
-      points: [p],
-      ...(this.tool === "pen" && { stroke: { width: 2, color: 'black' } })
-    };
+    this.points.push(p);
   }
 
   pointerMove(p: Point) {
-    if (this.tool === "pen" && this.size % this.smoothFactor !== 0) {
-      this.currentEvent?.points.pop();
+    if (this.tool !== "eraser" && this.size % this.smoothFactor !== 0) {
+      this.points.pop();
     }
-    this.currentEvent?.points.push(p);
+    this.points.push(p);
     this.size++;
   }
 
   pointerUp(p: Point) {
     this.started = false;
-    if (this.currentEvent) {
-      this.currentEvent.points.push(p);
-      this.Events.push(this.currentEvent);
-      this.currentEvent.points = [];
-    }
+    const currentEvent = {
+      type: this.tool,
+      startTime: p.time,
+      points: this.points,
+      ...(this.tool === "pen" && { stroke: { width: 2, color: 'black' } })
+    };
+    this.Events.push(currentEvent);
     this.size = 0;
+    this.points = [];
+    console.log("this.Events", this.Events);
   }
 }
