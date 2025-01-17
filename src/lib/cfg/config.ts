202d21dfce448c05d032fb8ef923fb2358afca6d
arkochan <najmussakibarko@gmail.com>
2025-01-18 04:57:25 +0600

    Added default keybindings

---
+++ src/lib/cfg/config.ts
@@ -0,0 +1,22 @@
+// src/config/keybindings.ts
+export const config = {
+  keybindings: {
+    tools: {
+      pen: "P",
+      eraser: "E",
+      rectangle: "R",
+      circle: "C",
+      selection: "S",
+    },
+    actions: {
+      undo: "Ctrl+Z",
+      redo: "Ctrl+Y",
+      clear: "Ctrl+C",
+    },
+    media: {
+      record: "Ctrl+R",
+      pause: "Ctrl+P",
+      stop: "Ctrl+S",
+    },
+  }
+};
