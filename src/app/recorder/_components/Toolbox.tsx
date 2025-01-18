c941e129a0672b5ec6665ddbd808166d3c24583c
arkochan <najmussakibarko@gmail.com>
2025-01-18 13:59:13 +0600

    takes canvas color width event handler

--- src/app/recorder/_components/Toolbox.tsx
+++ src/app/recorder/_components/Toolbox.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { cn } from '@/lib/utils/tailwind';
 import ButtonTB from '@/components/ButtonTB';
 import { useStore } from '../_store/useStore'; // Adjust the import path as necessary
+import Tweaker from './ToolTweak';
 
 const tools = [
   // { name: 'mouse', svgSrc: '/mouse-pointer-2.svg' },
@@ -11,23 +12,36 @@ const tools = [
   { name: 'circle', svgSrc: '/circle.svg' },
 ];
 
-export default function Toolbox({ className, changeTool }: { className?: string, changeTool: (tool: string) => void }) {
+export default function Toolbox({ className, changeTool, setColor, setWidth }: { className?: string, changeTool: (tool: string) => void, setColor: (s: string) => void, setWidth: (n: number) => void }) {
   const setCurrentTool = useStore((state) => state.setCurrentTool);
   const currentTool = useStore((state) => state.currentTool);
+  const [tweakerVisible, setTweakerVisible] = React.useState(false);
   function handleToolChange(tool: string) {
+    if (tool === 'eraser') {
+      setTweakerVisible(false);
+    }
+    else if (currentTool === tool) {
+      setTweakerVisible(!tweakerVisible);
+    }
+    else { setTweakerVisible(true); }
     changeTool(tool);
     setCurrentTool(tool);
   }
   return (
-    <div className={cn("flex-col h-fit", className)}>
-      {tools.map((tool) => (
-        <ButtonTB
-          key={tool.name}
-          active={currentTool === tool.name}
-          svgSrc={tool.svgSrc}
-          onClick={() => handleToolChange(tool.name)}
-        />
-      ))}
-    </div>
+    <>
+      <div className={cn("flex-col h-fit tool-container-vertical", className)}>
+        {tools.map((tool) => (
+          <ButtonTB
+            key={tool.name}
+            active={currentTool === tool.name}
+            svgSrc={tool.svgSrc}
+            onClick={() => handleToolChange(tool.name)}
+          />
+        ))}
+      </div>
+      <div className={cn('absolute top-10 left-20 h-fit', tweakerVisible ? "block" : "hidden")}>
+        <Tweaker setColor={setColor} setWidth={setWidth} />
+      </div>
+    </>
   );
 }
--- src/app/recorder/page.tsx
+++ src/app/recorder/page.tsx
@@ -88,10 +88,11 @@ export default function page() {
       <Presentation className="-z-10 shadow-md absolute rounded-xl right-0 top-1/2 -translate-y-full border h-52 w-72 flex justify-center items-center" />
       <Camera className="absolute bg-white rounded-full w-32 h-32 right-10 bottom-10" />
       <div className="absolute -translate-y-1/2 top-1/2 left-4">
-        <Toolbox changeTool={changeTool} className="tool-container-vertical  " />
+        <Toolbox setWidth={(width) => canvas.setWidth(width)} setColor={(c) => canvas.setColor(c)} changeTool={changeTool} className="  " />
         <StateToolbox executeAction={executeAction} className="tool-container-vertical mt-2 " />
       </div>
       <InfoBar closeModal={closeModal} setModalOpen={setModalOpen} className="tool-container-horizontal absolute right-4 top-4" />
+
       {/* <Controls className="tool-container-horizontal absolute bottom-10 left-10" /> */}
       {/* <AudioRecorder className="tool-container-horizontal absolute bottom-10 left-10" /> */}
 
